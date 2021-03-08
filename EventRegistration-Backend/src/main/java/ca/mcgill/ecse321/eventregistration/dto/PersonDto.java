ac9e243081d65f109700ff4635cae321ecdf3062
JulietteDebray <juliette.debray@mail.mcgill.ca>
2021-03-09 00:40:35 +0100

    from last tutorial

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/dto/PersonDto.java
@@ -0,0 +1,38 @@
+package ca.mcgill.ecse321.eventregistration.dto;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PersonDto {
+
+    //no setters bc we (want to only) use the constructors
+
+    private String name;
+    private List<EventDto> events;
+
+    public PersonDto() {
+    }
+
+    @SuppressWarnings("unchecked")
+    public PersonDto(String name) {
+        this(name, Collections.EMPTY_LIST);
+    }
+
+    public PersonDto(String name, List<EventDto> arrayList) {
+        this.name = name;
+        this.events = arrayList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<EventDto> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<EventDto> events) {
+        this.events = events;
+    }
+
+}
